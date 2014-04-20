5aa55a4030fc3dba5ff5e503981768fe4c6d45b1
Jack Gilliam <Jack-Gilliam@hotmail.com>
2014-04-20 14:47:59 -0600

    Changed images to be lazy loaded and cached

--- src/blockamon/objects/images/ImageLoader.java
+++ src/blockamon/objects/images/ImageLoader.java
@@ -5,6 +5,8 @@ import blockamon.objects.data.ImageData;
 import javax.swing.*;
 import java.awt.Image;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * User: Jack's Computer
@@ -12,15 +14,35 @@ import java.net.URL;
  * Time: 1:40 PM
  */
 public class ImageLoader implements IImageLoader {
+
+    private Map<String, Image> _loadedImages;
+
+    public ImageLoader() {
+        _loadedImages = new HashMap<String, Image>();
+    }
+
     public Image loadImage(ImageData imageData) {
         String imagePath = imageData.getFullFilePath();
+        Image loadedImage = findLoadedImage(imagePath);
+        if(loadedImage != null) {
+            return loadedImage;
+        }
+
         URL imgURL = getClass().getResource(imagePath);
         if (imgURL != null) {
             Image icon = new ImageIcon(imgURL).getImage();
             System.out.println("Loaded image: " + imagePath);
+            _loadedImages.put(imagePath, icon);
             return icon;
         } else {
             System.err.println("Couldn't find file: " + imagePath);
+        }
+        return null;
+    }
+
+    public Image findLoadedImage(String imagePath) {
+        if(_loadedImages.containsKey(imagePath)) {
+            return _loadedImages.get(imagePath);
         }
         return null;
     }
